3f386d97695f580cff2b4d739ff7beba5adc1986
DavidGheorghiu <47111164+DavidGheorghiu@users.noreply.github.com>
2019-08-08 12:05:59 +0100

    Update feedforward_neural_network.py

--- feedforward_neural_network.py
+++ feedforward_neural_network.py
@@ -3,14 +3,14 @@ import numpy
 #our data
 #[height, age, gender(0=male, 1=female)]
 data = [
-    [180, 21, 0],
-    [169, 17, 1],
-    [153, 13, 1],
-    [176, 20, 0],
-    [172, 35, 1],
-    [195, 26, 0],
-    [160, 16, 1],
-    [180, 41, 0]
+    [1.83, 5.7, 0],
+    [1.69, 1.7, 1],
+    [1.53, 1.3, 1],
+    [1.76, 2.0, 0],
+    [1.72, 3.5, 1],
+    [1.95, 2.6, 0],
+    [1.60, 1.6, 1],
+    [1.80, 4.1, 0]
 ]
 
 # sigmoid function (activiation function)
@@ -28,9 +28,9 @@ def d_cost(prediction, target):
 #train the computer giving it our data
 def train():
     #initially pick random numbers for our weights and bias
-    w1 = numpy.random.randn()*0.1
-    w2 = numpy.random.randn()*0.1
-    b = numpy.random.randn()*0.1
+    w1 = numpy.random.randn()
+    w2 = numpy.random.randn()
+    b = numpy.random.randn()
 
     #how fast we want the computer to learn
     learning_rate = 0.15
@@ -40,7 +40,7 @@ def train():
         
         # apply weights to data entry
         z = randomDataEntry[0]*w1 + randomDataEntry[1]*w2 + b
-        
+
         #get prediction and target
         prediction = sigmoid(z)
         target = randomDataEntry[2]
@@ -71,10 +71,10 @@ def train():
     #finally we return our adjusted weights and bias
     return [w1, w2, b]
 
-#test prediction on
+#test prediction
 learned = train()
 
 #data on female so our prediction should be close to 1
-z = 160*learned[0] + 21*learned[1] + learned[2]
+z = 1.76*learned[0] + 3.1*learned[1] + learned[2]
 prediction = sigmoid(z)
 print(prediction)
